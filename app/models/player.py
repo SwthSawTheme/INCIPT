a052cdfb9c72081fa20a945c126da31e17c6ead8
SwthSawTheme <sawtheme1@gmail.com>
2024-10-04 20:49:25 -0300

    Criado classe stalker

---
+++ app/models/player.py
@@ -0,0 +1,23 @@
+
+class Player(object):
+
+    def __init__(self, obsession_level:int, hacking_skill:int, gathered_data:list):
+        self.obsession_level = 0
+        self.hacking_level = 0
+        self.gathered_data = []
+
+    def hack_device(self):
+        # Método que permite o stalker invadir o dispositivo da mulher.
+        pass
+
+    def extract_photos(self):
+        # Extrai fotos da vítima.
+        pass
+
+    def monitor_activity(self):
+        # Observa as atividades da mulher.
+        pass
+
+    def increase_obsession(self):
+        # Aumenta o nível de obsessão conforme novas informações são obtidas.
+        pass
