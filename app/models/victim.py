f6d55b733806df9ba43fe7f468f6518a66ac84d6
SwthSawTheme <sawtheme1@gmail.com>
2024-10-04 21:00:18 -0300

    Classe victim criado

---
+++ app/models/victim.py
@@ -0,0 +1,22 @@
+
+class Victim(object)?
+    
+    def __init__(self,age:int, name:str, occupation:str, college:str, trauma_history:str, friends:list):
+        self.age = age
+        self.name = name
+        self.occupation = occupation
+        self.college = college
+        self.trauma_history = trauma_history
+        self.friends = friends
+
+    def work(self):
+        # Realiza suas funções nos empregos.
+        pass
+
+    def study(self):
+        # Frequenta a faculdade de medicina.
+        pass
+
+    def contact_friend(self):
+        # Conversa com sua amiga.
+        pass
